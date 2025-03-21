397a4e594ee7382a2c73f41282bfc0a628d15561
Hunter Mailloux <huntermmailloux@gmail.com>
2025-03-20 20:54:22 -0400

    Adding word cloud

--- src/app/anaylsis/anaylsis.component.ts
+++ src/app/anaylsis/anaylsis.component.ts
@@ -12,7 +12,13 @@ import { StockService } from '../../services/stock.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { NgxEchartsModule, NGX_ECHARTS_CONFIG } from 'ngx-echarts';
 import * as echarts from 'echarts';
+import * as d3 from 'd3';
+import cloud from 'd3-cloud';
 
+interface Word {
+  text: string;
+  size: number;
+}
 
 @Component({
   selector: 'app-anaylsis',
@@ -44,6 +50,8 @@ import * as echarts from 'echarts';
   ]
 })
 export class AnaylsisComponent implements OnInit {
+  stocks: any[] = ['AA', 'AAL', 'AAPL', 'ABBV', 'ABNB', 'ACHR', 'ACN', 'ADBE', 'ADI', 'ADM', 'ADP', 'ADSK', 'AEE', 'AEP', 'AES', 'AFL', 'AIG', 'AKAM', 'ALB', 'ALGN', 'ALLE', 'AMAT', 'AMC', 'AMCR', 'AMD', 'AME', 'AMGN', 'AMP', 'AMT', 'AMTM', 'AMZN', 'ANET', 'ANSS', 'AON', 'AOS', 'APA', 'APD', 'APH', 'APTV', 'ASTS', 'ATO', 'AVB', 'AVGO', 'AVY', 'AWK', 'AXON', 'AXP', 'AZO', 'BA', 'BABA', 'BAC', 'BALL', 'BAX', 'BBY', 'BDX', 'BEN', 'BG', 'BIIB', 'BK', 'BKNG', 'BKR', 'BLDR', 'BLK', 'BMY', 'BR', 'BSX', 'BX', 'BXP', 'CAG', 'CAH', 'CARR', 'CB', 'CBOE', 'CBRE', 'CC', 'CCI', 'CCL', 'CDNS', 'CDW', 'CE', 'CEG', 'CF', 'CFG', 'CHD', 'CHRW', 'CHTR', 'CI', 'CL', 'CLOV', 'CLX', 'CMCSA', 'CME', 'CMG', 'CMI', 'CMS', 'CNC', 'CNP', 'COF', 'COO', 'COP', 'COR', 'COST', 'CPB', 'CPRT', 'CPT', 'CRL', 'CRM', 'CRWD', 'CSCO', 'CSGP', 'CSX', 'CTAS', 'CTRA', 'CTSH', 'CTVA', 'CVS', 'CVX', 'CZR', 'DAL', 'DE', 'DECK', 'DELL', 'DFS', 'DG', 'DGX', 'DHI', 'DHR', 'DIS', 'DJT', 'DLR', 'DLTR', 'DM', 'DOC', 'DOCU', 'DOV', 'DOW', 'DPZ', 'DRI', 'DTE', 'DUK', 'DVA', 'DVN', 'DXCM', 'EA', 'EBAY', 'ECL', 'ED', 'EFX', 'EG', 'EIX', 'EL', 'ELV', 'EMN', 'EMR', 'ENPH', 'EOG', 'EPAM', 'EQIX', 'EQR', 'EQT', 'ERIE', 'ES', 'ESS', 'ETN', 'ETR', 'EU', 'EVRG', 'EW', 'EXC', 'EXPD', 'EXPE', 'EXR', 'FANG', 'FAST', 'FCX', 'FDS', 'FDX', 'FE', 'FFIV', 'FI', 'FICO', 'FIS', 'FITB', 'FMC', 'FOX', 'FOXA', 'FRT', 'FSLR', 'FTNT', 'GAP', 'GD', 'GDDY', 'GE', 'GEHC', 'GEN', 'GEV', 'GILD', 'GIS', 'GL', 'GLD', 'GLW', 'GM', 'GME', 'GNRC', 'GOOG', 'GOOGL', 'GPC', 'GPN', 'GRMN', 'GS', 'GWW', 'HAL', 'HBAN', 'HCA', 'HD', 'HES', 'HIG', 'HII', 'HLT', 'HOLX', 'HON', 'HOOD', 'HPE', 'HPQ', 'HRL', 'HSIC', 'HST', 'HSY', 'HUBB', 'HUM', 'HWM', 'IBM', 'ICE', 'IEX', 'IFF', 'INCY', 'INTC', 'INTU', 'INVH', 'IP', 'IPA', 'IPG', 'IR', 'IRM', 'ISRG', 'ITW', 'IVZ', 'IWM', 'JBHT', 'JBL', 'JCI', 'JD', 'JKHY', 'JNJ', 'JNPR', 'JPM', 'KDP', 'KEYS', 'KHC', 'KIM', 'KKR', 'KLAC', 'KMB', 'KMI', 'KMX', 'KO', 'KR', 'KVUE', 'LDOS', 'LEN', 'LH', 'LHX', 'LIN', 'LKQ', 'LLY', 'LMT', 'LNT', 'LOGC', 'LRCX', 'LULU', 'LUNR', 'LVS', 'LW', 'LYB', 'LYV', 'MAA', 'MAR', 'MAS', 'MCD', 'MCHP', 'MCK', 'MCO', 'MDLZ', 'MDT', 'MET', 'META', 'MGM', 'MHK', 'MKC', 'MKTX', 'MLM', 'MMM', 'MNST', 'MO', 'MOH', 'MOS', 'MPC', 'MPWR', 'MRK', 'MRNA', 'MS', 'MSCI', 'MSFT', 'MSI', 'MSTR', 'MTB', 'MTCH', 'MTD', 'MU', 'NCLH', 'NDAQ', 'NDSN', 'NEE', 'NEM', 'NFLX', 'NIO', 'NKE', 'NOC', 'NRG', 'NSC', 'NTAP', 'NTRS', 'NUE', 'NVDA', 'NVR', 'NWS', 'NXPI', 'NYT', 'ODFL', 'OKE', 'OKLO', 'OMC', 'ORCL', 'ORLY', 'OTIS', 'OXY', 'PANW', 'PARA', 'PAYC', 'PAYX', 'PCAR', 'PCG', 'PEG', 'PEP', 'PFE', 'PFG', 'PG', 'PGR', 'PH', 'PHM', 'PLD', 'PLTR', 'PM', 'PNC', 'PNW', 'PODD', 'POOL', 'PPG', 'PR', 'PRU', 'PSA', 'PSX', 'PTC', 'PTON', 'PWR', 'PYPL', 'QBTS', 'QCOM', 'QQQ', 'QRVO', 'RCL', 'RDDT', 'REG', 'REGN', 'RF', 'RGTI', 'RIVN', 'RJF', 'RKLB', 'RL', 'RMD', 'ROK', 'ROL', 'ROP', 'ROST', 'RR', 'RTX', 'SBAC', 'SBUX', 'SCHW', 'SHW', 'SJM', 'SLB', 'SMCI', 'SMTC', 'SNPS', 'SOFI', 'SOLV', 'SPG', 'SPGI', 'SPY', 'SQQQ', 'SRE', 'STE', 'STLD', 'STX', 'STZ', 'SW', 'SWK', 'SWKS', 'SYF', 'SYK', 'SYY', 'TAP', 'TDG', 'TDY', 'TECH', 'TEL', 'TER', 'TFC', 'TFX', 'TGT', 'TJX', 'TLT', 'TMO', 'TMUS', 'TPL', 'TPR', 'TQQQ', 'TRGP', 'TROW', 'TRV', 'TSCO', 'TSLA', 'TSM', 'TSN', 'TT', 'TTD', 'TTWO', 'TWLO', 'TXN', 'TXT', 'TYL', 'UAL', 'UBER', 'UHS', 'ULTA', 'UNH', 'UNP', 'UPS', 'URI', 'USB', 'VICI', 'VLO', 'VOO', 'VRSN', 'VRTX', 'VST', 'VTR', 'VTRS', 'VZ', 'WAB', 'WAT', 'WBA', 'WBD', 'WDC', 'WEC', 'WEN', 'WFC', 'WH', 'WM', 'WMB', 'WMT', 'WST', 'WW', 'WY', 'WYNN', 'XEL', 'XOM', 'XYL', 'YUM', 'ZBH', 'ZBRA', 'ZTS'];
+
   stockTicker: string | null = '';
   loading: boolean = true;
   private _count: number = 0;
@@ -63,6 +71,8 @@ export class AnaylsisComponent implements OnInit {
   sentDistributionOptions: any;
   postVolumeOptions: any;
   isMonthlyView: boolean = true;
+
+  wordData: { [key: string]: any[] } = { positive: [], neutral: [], negative: [] };
   
   get count(): number {
     return this._count;
@@ -70,7 +80,7 @@ export class AnaylsisComponent implements OnInit {
 
   set count(value: number) {
     this._count = value;
-    if (this.count == 6) {
+    if (this.count == 7) {
       this.loading = false;
     }
   }
@@ -91,6 +101,7 @@ export class AnaylsisComponent implements OnInit {
           this.generateSentOverTime(this.posts);
           this.generateSentimentDistribution(this.posts);
           this.generatePostVolumeChart(this.posts);
+          this.generateWordClouds(this.posts);
         }
         this.count++;
       }
@@ -385,6 +396,124 @@ export class AnaylsisComponent implements OnInit {
     };
   
     this.count++;
+  }
+
+  generateWordClouds(posts: any) {
+    const stocksLower = this.stocks?.map(stock => stock.toLowerCase()) || [];
+    let wordCount: { [key: string]: { [word: string]: number } } = {
+      positive: {}, neutral: {}, negative: {}
+    };
+
+    posts.forEach((post: { sentiment: any; preprocessedPost: string; }) => {
+      let sentiment = post.sentiment;
+      let words = post.preprocessedPost.split(/\s+/); // Basic tokenization by space
+
+      let sentimentKey = sentiment === 1 ? 'positive' : sentiment === 0 ? 'neutral' : 'negative';
+
+      words.forEach(word => {
+        word = word.toLowerCase().replace(/[^a-z0-9]/g, ''); // Normalize words (remove punctuation)
+
+        if (/\d/.test(word)) {
+          return; // Skip words containing numbers
+        }
+
+        if (stocksLower.includes(word)) {
+          return; // Skip words found in this.stocks
+        }
+
+        if (word.length > 2) { // Filter out very short words
+          if (!wordCount[sentimentKey][word]) {
+            wordCount[sentimentKey][word] = 0;
+          }
+          wordCount[sentimentKey][word]++;
+        }
+      });
+    });
+
+    // Convert word frequency to array format for D3 with improved scaling
+    ['positive', 'neutral', 'negative'].forEach(sentimentKey => {
+      let sortedWords = Object.entries(wordCount[sentimentKey])
+        .sort((a, b) => b[1] - a[1]) // Sort by frequency (descending)
+        .slice(0, 1500); // Take top 100 words
+
+      let maxFreq = sortedWords.length ? sortedWords[0][1] : 1; // Max word frequency
+      let minFreq = sortedWords.length ? sortedWords[sortedWords.length - 1][1] : 1; // Min word frequency
+
+      this.wordData[sentimentKey] = sortedWords.map(([word, freq]) => {
+        // Exponential scaling for better contrast
+        let scaledSize = 10 + (Math.pow(freq / maxFreq, 0.5) * 50); // Root scaling for balanced effect
+
+        return { text: word, size: scaledSize, frequency: freq };
+      });
+
+      this.createWordCloud(`wordCloud${sentimentKey.charAt(0).toUpperCase() + sentimentKey.slice(1)}`, this.wordData[sentimentKey]);
+    });
+    this.count++;
+  }
+
+  createWordCloud(elementId: string, words: any[]) {
+    const width = 550, height = 350;
+    
+    const layout = cloud()
+      .size([width, height])
+      .words(words)
+      .padding(5)
+      .rotate(() => (Math.random() > 0.5 ? 0 : 90))  // Random rotation
+      .fontSize(d => d.size ?? 10)
+      .on('end', draw);
+
+      let tooltip = d3.select("body") // Create tooltip div
+      .append("div")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("background", "rgba(0,0,0,0.8)")
+      .style("color", "#fff")
+      .style("padding", "5px 10px")
+      .style("border-radius", "5px")
+      .style("font-size", "14px")
+      .style("top", "-9999px")
+      .style("left", "-9999px")
+
+    layout.start();
+
+    function draw(words: any[]) {
+      d3.select(`#${elementId}`).html(''); // Clear previous cloud
+      d3.select("body").selectAll("div.tooltip").remove();
+      const svg = d3.select(`#${elementId}`)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', `translate(${width / 2},${height / 2})`);
+
+      svg.selectAll('text')
+        .data(words)
+        .enter()
+        .append('text')
+        .style('font-size', d => `${d.size}px`)
+        .style('fill', () => d3.schemeCategory10[Math.floor(Math.random() * 10)])
+        .attr('text-anchor', 'middle')
+        .attr('transform', d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
+        .text(d => d.text)
+        .on("mouseover", function (event, d) {
+          tooltip.style("visibility", "visible")
+            .text(`${d.text}: ${d.frequency} times`);
+            d3.select(this).style("cursor", "pointer");
+        })
+        .on("mousemove", function (event) {
+          tooltip.style("top", `${event.pageY - 20}px`)
+            .style("left", `${event.pageX + 10}px`);
+        })
+        .on("mouseout", function () {
+          tooltip.style("visibility", "hidden")
+                 .style("top", "-9999px")  // Move it off-screen
+                 .style("left", "-9999px"); // Move it off-screen
+        });
+    }
+  }
+
+  hasWords(sentiment: string): boolean {
+    return (this.wordData[sentiment] ?? []).length > 0;
   }
 
   backToHome() {
